07d9a9145eb6280ea9dcffb45cd682889dd8063b
kawamataryou <ba068082@gmail.com>
2020-12-20 12:57:45 +0900

    chore: fix format

--- src/GraphQLClient.elm
+++ src/GraphQLClient.elm
@@ -1,9 +1,15 @@
 module GraphQLClient exposing (makeGraphQLQuery)
+
 import Graphql.Http
 import Graphql.Operation exposing (RootQuery)
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
+
+
 graphql_url : String
-graphql_url = "https://graphql-pokemon2.vercel.app/"
+graphql_url =
+    "https://graphql-pokemon2.vercel.app/"
+
+
 makeGraphQLQuery : SelectionSet decodesTo RootQuery -> (Result (Graphql.Http.Error decodesTo) decodesTo -> msg) -> Cmd msg
 makeGraphQLQuery query decodesTo =
     query
